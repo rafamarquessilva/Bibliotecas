2c40e9358d38c63219200b530fa77dfb43a6317c
Rafael Marques <rifacrp100@hotmail.com>
2023-01-16 14:57:12 -0300

    Melhoria para facilitar a manutenção da lógica de validação de senha

---
+++ bytebank_Modelos/bytebank.Modelos/ADM/Utilitario/AutenticacaoUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bytebank_Modelos.bytebank.Modelos.ADM.Utilitario
+{
+    internal class AutenticacaoUtil
+    {
+        internal bool ValidarSenha(string senhaverdadeira, string senhatentativa)
+        {
+            return senhatentativa.Equals(senhaverdadeira);
+        }
+    }
+}
